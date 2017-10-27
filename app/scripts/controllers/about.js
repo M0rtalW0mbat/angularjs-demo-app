5e1426fdea8065bad337ba37b80b53a26320a974
mikhail.mikhalchuk <mikhail.mikhalchuk@gmail.com>
2017-10-27 15:44:23 +0700

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myArticleAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the myArticleAppApp
+ */
+angular.module('myArticleAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myArticleAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myArticleAppApp
+ */
+angular.module('myArticleAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
