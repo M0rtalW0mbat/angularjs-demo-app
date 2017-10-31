bb5cbf37bb5852ab2bb5fdd0fdc905800d7769b5
mikhail.mikhalchuk <mikhail.mikhalchuk@gmail.com>
2017-10-31 23:45:20 +0700

    General view & controller

--- app/scripts/controllers/articles.js
+++ app/scripts/controllers/articles.js
@@ -6,48 +6,50 @@
  * Controller of the myArticleApp
  */
 
+// TODO try/catch?
+// TODO rename factories
+// TODO moment.js for Date format and tz offset(utc date on back-end)
+// TODO docs
+
 (function() {
     'use strict';
     angular
         .module('myArticleApp')
         .controller('ArticlesCtrl', ArticlesCtrl);
 
-    ArticlesCtrl.$inject = [];
+    ArticlesCtrl.$inject = ['articlesFactory', 'articleFactory', '$location'];
 
-    function ArticlesCtrl() {
+    function ArticlesCtrl(articlesFactory, articleFactory, $location) {
         var vm = this;
 
-        vm.articles = [{
-            id: 1,
-            title: 'Mars colonization program',
-            creationDate: '2017.10.27'
-        }];
+        // TODO loading screen?
+        // TODO move to onLoad? move to activate?
+        vm.articles = articlesFactory.query();
 
         activate();
 
         ////////////////
 
         function activate() {
-            vm.getArticles = getArticles;
             vm.createArticle = createArticle;
             vm.editArticle = editArticle;
             vm.deleteArticle = deleteArticle;
-        };
-
-        function getArticles() {
-            // TODO factory
-        };
+        }
 
         function createArticle() {
-            // TODO
-        };
+            $location.path('/create');
+        }
 
-        function editArticle(id) {
-            // TODO
-        };
+        function editArticle(Id) {
+            $location.path('/edit/' + Id); //TODO explicitly specify routeParams
+        }
 
-        function deleteArticle(id) {
-            // TODO
-        };
-    };
+        function deleteArticle(Id) {
+            articlesFactory.delete({
+                Id: Id
+            }).$promise.then(function(result) {
+                vm.articles = articlesFactory.query();
+            });
+        }
+    }
 })();
